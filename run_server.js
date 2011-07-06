85f0ff787fa5b6dc056108bd7c7c213e27c7b819
pangratz <pangratz666@gmail.com>
2011-07-06 23:15:08 +0200

    implemented CouchDB as development backend, because using GAE is very slow in combination with the Sproutcore's sc-sever proxy

--- run_server.js
+++ run_server.js
@@ -1,47 +1,93 @@
 var sys = require('sys');
 var http = require('http');
-var app = require('express').createServer();
+var express = require('express');
+var app = express.createServer();
+app.use(express.bodyParser());
 
-var networkplan = {};
+var CouchClient = require('couch-client');
+var oeffinpc = new CouchClient('http://localhost:5984/oeffinpc');
 
-networkplan['linz'] = {
-	"imageUrl": "static/oeffi_npc/en/build/source/resources/images/linz_bw.png",
-	"imageWidth": 1114,
-	"imageHeight": 1618,
-	"networkId": "linz",
-	"planId": "linz",
-	"entries": [
-		{
-			"stationId": "123",
-			"name": "Schumpeterstrasse",
-			"x": 100,
-			"y": 200
-		},
-		{
-			"stationId": 456,
-			"name": "Hauptbahnhof",
-			"x": 124,
-			"y": 50
-		}
-	]
-};
-networkplan['bonn'] = {
-	"imageUrl": "static/oeffi_npc/en/build/source/resources/images/bonn_schnellverkehr_bw.png",
-	"imageWidth": 2338,
-	"imageHeight": 1653,
-	"networkId": "bonn",
-	"planId": "bonn"
-};
-
-app.get('/networkplan', function(req, res){
-	var networkplanArray = [];
-	networkplanArray.push( networkplan['linz'] );
-	networkplanArray.push( networkplan['bonn'] );
-	res.send( networkplanArray );
+app.get('/networkplans', function(req, res){
+	oeffinpc.view('/oeffinpc/_design/oeffinpc/_view/all_networkplans', {}, function(err, doc){
+		var networkPlans = doc.rows.map(function(val){
+			return val.value;
+		});
+		res.send(networkPlans);
+	});
 });
 
-app.get('/networkplan/:city', function(req, res){
-	res.send( [ networkplan[req.params.city] ] );
+app.put('/networkplans/:key', function(req, res){
+	res.send('ok');
+});
+
+app.post('/networkplans/:key', function(req, res){
+	var body = req.body;
+	var key = req.params.key;
+	body['_id'] = undefined;
+	body['networkPlanKey'] = key;
+	body['x'] = body.x ? body.x : 0;
+	body['y'] = body.y ? body.y : 0;
+	oeffinpc.save(body, function(err, doc){
+		res.send(doc);
+	});
+});
+
+app.get('/networkplans/:key', function(req, res){
+	var key = req.params.key;
+	oeffinpc.view('/oeffinpc/_design/oeffinpc/_view/all_networkplans?key="'+key+'"', {}, function(err, doc){
+		var rows = doc.rows;
+		if (rows && rows.length == 1) {
+			var networkPlan = rows[0].value;
+			oeffinpc.view('/oeffinpc/_design/oeffinpc/_view/all_networkplanentries?key="'+key+'"', {}, function(err, doc){
+				if (doc && doc.rows) {
+					networkPlan['entries'] = doc.rows.map(function(val){
+						return val.value;
+					});
+				}
+				res.send(networkPlan);
+			});
+			return;
+		}
+		res.send(null);
+	});
+});
+
+app.get('/networkplanentries', function(req, res){
+	oeffinpc.view('/oeffinpc/_design/oeffinpc/_view/networkplanentry', {}, function(err, doc){
+		if (doc.rows) {
+			var entries = doc.rows.map(function(val){
+				return val.value;
+			});
+			res.send(entries);
+		}
+		res.send('no thing');
+	});
+});
+
+app.get('/networkplanentries/:key', function(req, res){
+	var key = req.params.key;
+	oeffinpc.view('/oeffinpc/_design/oeffinpc/_view/networkplanentry?key="'+key+'"', {}, function(err, doc){
+		var rows = doc.rows;
+		if (rows) {
+			res.send(rows[0].value);
+		}
+		res.send(null);
+	});
+});
+
+app.put('/networkplanentries/:key', function(req, res){
+	var body = req.body;
+	var key = req.params.key;
+	oeffinpc.save(body, function(err, doc){
+		res.send(doc);
+	});
+});
+
+app.del('/networkplanentries/:key', function(req, res){
+	var key = req.params.key;
+	oeffinpc.remove(key, function(err, doc){
+		res.send(doc);
+	});
 });
 
 app.listen(3000);
